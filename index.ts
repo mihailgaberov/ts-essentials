7f4c18bf964d98c4c30d0a73ac1ea1c41ddfd3d9
mihailgaberov <mihail.gaberov@gmail.com>
2024-12-10 12:33:07 +0100

    :sparkles: add examples

--- index.ts
+++ index.ts
@@ -21,3 +21,25 @@ const getRectangleArea = (rectangle: Rectangle) => {
 const getRectanglePerimeter = (rectangle: Rectangle) => {
   return 2 * (rectangle.width + rectangle.height);
 };
+
+type ShoppingCart = {
+  userId: string;
+  items: string[];
+};
+
+const processCart = (cart: ShoppingCart) => {
+  // Do something with the cart in here
+};
+
+processCart({
+  userId: "user123",
+  items: ["item1", "item2", "item3"],
+});
+
+// named tuple
+const goToLocation = (
+  coordinates: [latitude: number, longitude: number, elevation?: number]
+) => {};
+
+// alternatively, non-named tuple
+const goToLocation1 = (coordinates: [number, number, number?]) => {};
